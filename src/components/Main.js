cefb85dbea6f28df2c513edcdb31c4656865ebaf
bawantha <bawantha@orelit.com>
2023-04-01 17:48:24 +0530

    config saga middleware

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,13 @@
-import { addToCart } from "../redux/action";
+import { addToCart, removeToCart, emptyCart } from "../redux/action";
+import { productList } from "../redux/productAction";
 import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 function Main() {
   const dispatch = useDispatch();
+
+  let productData = useSelector((state) => state.productData);
+  console.warn(productData);
 
   const product = {
     name: "iphone",
@@ -12,6 +17,21 @@ function Main() {
   return (
     <div className="App">
       <button onClick={() => dispatch(addToCart(product))}>Addd to cart</button>
+
+      <br />
+      <div>
+        <button onClick={() => dispatch(removeToCart(product.name))}>
+          Remove from cart
+        </button>
+        <button onClick={() => dispatch(emptyCart())}>Empty Cart</button>
+      </div>
+      <div>
+        <button onClick={() => dispatch(productList())}>
+          Get product list
+        </button>
+      </div>
+
+      <h1>Product List</h1>
     </div>
   );
 }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,11 +1,18 @@
-import { ADD_TO_CART } from "./constant";
+import { ADD_TO_CART, REMOVE_TO_CART, EMPTY_CART } from "./constant";
 
 export const cartData = (data = [], action) => {
   switch (action.type) {
     case ADD_TO_CART:
-      console.log("ADD_TO_CART Condition", action);
       return [...data, action.data];
+
+    case REMOVE_TO_CART:
+      data.length = data.length ? data.length - 1 : [];
+      return [...data];
+
+    case EMPTY_CART:
+      data = [];
+      return [...data];
     default:
-      return [];
+      return data;
   }
 };
