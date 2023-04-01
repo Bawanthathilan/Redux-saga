0b41e583fac4deae539d77cef6f39ba65dcf0917
bawantha <bawantha@orelit.com>
2023-04-01 19:04:40 +0530

    call api with redux saga

--- src/redux/productSaga.js
+++ src/redux/productSaga.js
@@ -1,5 +1,19 @@
+import { takeEvery, put } from "redux-saga/effects";
+import { PRODUCT_LIST, SET_PRODUCT_LIST } from "./constant";
+
+function* getProducts() {
+  let data = yield fetch("http://localhost:5000/prodcuts");
+  data = yield data.json();
+  console.log("DATA", data);
+
+  yield put({
+    type: SET_PRODUCT_LIST,
+    data,
+  });
+}
+
 function* productSaga() {
-    
+  yield takeEvery(PRODUCT_LIST, getProducts);
 }
 
 export default productSaga;
